be385e0a4b92df9593a83f9350353276ccf9f308
VicGa <90812185+VicGa@users.noreply.github.com>
2022-04-23 00:53:25 -0300

    fix date design

--- DesignPrinciplesDocument/js/currentDate.js
+++ DesignPrinciplesDocument/js/currentDate.js
@@ -1,9 +1,10 @@
-const options = {year: 'numeric'};
+const options = { year: 'numeric' };
 
 let modified = document.querySelector('#currentDate');
+let copyright = document.querySelector('#currentYear');
 
 let current_year = new Date().toLocaleDateString('en-US', options);
 let mod_date = document.lastModified;
 
 modified.innerHTML = `Last Modified: ${mod_date}`;
-copyright.innerHTML = current_year;
+copyright.innerHTML = `${current_year}`;
