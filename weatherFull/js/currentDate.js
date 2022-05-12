b9de4999337b4c135799b8616875afd4233715aa
VicGa <90812185+VicGa@users.noreply.github.com>
2022-05-12 02:04:54 -0300

    change 4 by 6

--- weatherFull/js/currentDate.js
+++ weatherFull/js/currentDate.js
@@ -6,7 +6,7 @@ let lastModified = document.lastModified;
 document.getElementById('currentDate').textContent = lastModified;
 
 /*-----fraiday banner-------*/
-if (date.getDay() === 4) {
+if (date.getDay() === 6) {
     let bannerFriday = document.createElement('div');
     bannerFriday.classList.add('fridayBanner');
     bannerFriday.textContent = 'Saturday = > Preston Pancakes in the Park!At 9: 00 AM.';
